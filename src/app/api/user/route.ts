bf102d0a94d0c826d7735ac48b42237a07cffd18
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-21 14:04:42 -0300

    Create /user route

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import { type Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: Request) {
+  const body: Prisma.UserCreateInput & { organizationId: string } =
+    await request.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      name: body.name,
+      organizationId: body.organizationId,
+    },
+  });
+  return new Response(JSON.stringify(user));
+}
+
+export async function GET(request: Request) {
+  const users = await prisma.user.findMany({
+    include: {
+      organization: true,
+    },
+  });
+  return new Response(JSON.stringify(users));
+}
