a581a053c3bcb00a8840bb5254c2184c86035ad6
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-21 20:21:08 -0300

    Add DELETE route for job

---
+++ src/app/api/job/[id]/route.ts
@@ -0,0 +1,43 @@
+import { JobStatus, PrismaClient } from "@prisma/client";
+import { type Prisma } from "@prisma/client";
+import * as ec2 from "@app/services/ec2";
+
+const prisma = new PrismaClient();
+
+export async function GET(
+  request: Request,
+  { params }: { params: Prisma.JobSelect }
+) {
+  const projects = await prisma.job.findUniqueOrThrow({
+    where: {
+      id: params.id?.toString(),
+    },
+  });
+  return new Response(JSON.stringify(projects));
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Prisma.JobSelect }
+) {
+  const job = await prisma.job.findUniqueOrThrow({
+    where: {
+      id: params.id?.toString(),
+    },
+  });
+
+  await ec2.stopInstance({
+    instanceId: job.instanceId,
+  });
+
+  await prisma.job.update({
+    where: {
+      id: job.id,
+    },
+    data: {
+      status: JobStatus.DEPROVISIONED,
+    },
+  });
+
+  return new Response(JSON.stringify(job));
+}
