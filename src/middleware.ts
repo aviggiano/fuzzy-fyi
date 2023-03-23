2fc6d41607fa7705e75963afa8c03890e38877d9
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-23 15:26:10 -0300

    Fix auth

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareSupabaseClient({ req, res });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session?.user.email) {
+    return res;
+  } else {
+    const redirectUrl = req.nextUrl.clone();
+    redirectUrl.pathname = "/";
+    redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
+    return NextResponse.redirect(redirectUrl);
+  }
+}
+
+export const config = {
+  matcher: "/:path*",
+};
